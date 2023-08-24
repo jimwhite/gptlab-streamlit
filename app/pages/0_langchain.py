aad0dca5d8e168a8b6d7106aa34dff4ef90c9bb3
Jim White <jim@ifcx.org>
2023-08-23 18:05:11 -0700

    LLM with PromptTemplate and streaming callbacks workingish.

--- app/pages/0_langchain.py
+++ app/pages/0_langchain.py
@@ -1,4 +1,5 @@
 import os
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
@@ -6,7 +7,11 @@ from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.prompts import PromptTemplate
 import openai
 from pathlib import Path
+
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import ChatMessage
 import streamlit as st
+
 
 PROMPT_TEMPLATE_PATH = (Path(__file__).parent.parent / "prompt_template.txt").absolute()
 
@@ -72,39 +77,86 @@ with st.sidebar:
     model_name = st.selectbox("Choose the OpenAI chat model to use", get_model_names(openai_api_key))
 
 # Set up memory
-msgs = StreamlitChatMessageHistory(key="langchain_messages")
-memory = ConversationBufferMemory(chat_memory=msgs)
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    chat_memory=msgs, return_messages=True, memory_key="history", output_key="output"
+)
 if len(msgs.messages) == 0:
     # msgs.add_ai_message("How can I help you?")
+    # st.session_state["messages"] = [ChatMessage(role="assistant", content="How can I help you?")]
     msgs.add_ai_message("Hello.  Would you like to explore how to apply First UX Platform to your B2B SaaS?")
+    st.session_state.steps = {}
 
-view_messages = st.expander("View the message contents in session state")
+avatars = {"human": "user", "ai": "assistant"}
+for idx, msg in enumerate(msgs.messages):
+    with st.chat_message(avatars[msg.type]):
+        # Render intermediate steps if any were saved
+        for step in st.session_state.steps.get(str(idx), []):
+            if step[0].tool == "_Exception":
+                continue
+            with st.expander(f"✅ **{step[0].tool}**: {step[0].tool_input}"):
+                st.write(step[0].log)
+                st.write(f"**{step[1]}**")
+        st.write(msg.content)
 
-# Set up the LLMChain, passing in memory
 prompt = PromptTemplate(input_variables=["history", "human_input"], template=get_prompt_template())
-llm_chain = LLMChain(llm=ChatOpenAI(openai_api_key=openai_api_key, model_name=model_name), prompt=prompt, memory=memory)
+llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=model_name, streaming=True)
+llm_chain = LLMChain(llm=llm, prompt=prompt, memory=memory, output_key='output')
+print(f'llm_chain.input_keys: {llm_chain.input_keys}')
 
-# Render current messages from StreamlitChatMessageHistory
-for msg in msgs.messages:
-    st.chat_message(msg.type).write(msg.content)
+if chat_input := st.chat_input():
+    # st.session_state.messages.append(ChatMessage(role="user", content=chat_input))
+    st.chat_message("user").write(chat_input)
+    with st.chat_message("ai"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        print(f'memory: {memory.memory_variables}')
+        print(f'llm_chain.output_keys: {llm_chain.output_keys}')
+        response = llm_chain.run({'human_input':chat_input}, callbacks=[st_cb])
+        st.write(response)
+        # st.session_state.steps[str(len(msgs.messages) - 1)] = response["intermediate_steps"]
+        # st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
 
-# If user inputs a new prompt, generate and draw a new response
-if prompt := st.chat_input():
-    st.chat_message("human").write(prompt)
-    # Note: new messages are saved to history automatically by Langchain during run
-    response = llm_chain.run(prompt)
-    st.chat_message("ai").write(response)
 
-# From intro above 'You can view the contents of Session State in the expander below.'
-# # Draw the messages at the end, so newly generated ones show up immediately
-# with view_messages:
-#     """
-#     Memory initialized with:
-#     ```python
-#     msgs = StreamlitChatMessageHistory(key="langchain_messages")
-#     memory = ConversationBufferMemory(chat_memory=msgs)
-#     ```
+# class StreamHandler(BaseCallbackHandler):
+#     def __init__(self, container, initial_text=""):
+#         self.container = container
+#         self.text = initial_text
 
-#     Contents of `st.session_state.langchain_messages`:
-#     """
-#     view_messages.json(st.session_state.langchain_messages)
+#     def on_llm_new_token(self, token: str, **kwargs) -> None:
+#         self.text += token
+#         self.container.markdown(self.text)
+
+
+# # Set up the LLMChain, passing in memory
+# prompt = PromptTemplate(input_variables=["history", "human_input"], template=get_prompt_template())
+# stream_handler = StreamHandler(st.empty())
+# llm = ChatOpenAI(openai_api_key=openai_api_key, model_name=model_name, streaming=True)
+# llm_chain = LLMChain(llm=llm, prompt=prompt, memory=memory)
+
+# # If user inputs a new prompt, generate and draw a new response
+# if prompt := st.chat_input():
+#     st.chat_message("human").write(prompt)
+#     # Note: new messages are saved to history automatically by Langchain during run
+#     response = llm_chain.run(prompt)
+#     st.chat_message("ai").write(response)
+
+# if prompt := st.chat_input():
+#     st.chat_message("human").write(prompt)
+
+#     with st.chat_message("ai"):
+#         stream_handler = StreamHandler(st.empty())
+#         llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-4", streaming=True, callbacks=[stream_handler])
+#         response = llm_chain.run(prompt, callbacks=[stream_handler])
+#         st.chat_message("ai").write(response)
+#         # st.session_state.messages.append(ChatMessage(role="assistant", content=response.content))
+
+# llm = ChatOpenAI(model_name=model_name, openai_api_key=openai_api_key, streaming=True)
+# tools = [DuckDuckGoSearchRun(name="Search")]
+# chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools)
+# executor = AgentExecutor.from_agent_and_tools(
+#     agent=chat_agent,
+#     tools=tools,
+#     memory=memory,
+#     return_intermediate_steps=True,
+#     handle_parsing_errors=True,
+# )
